a9490d46229a6c8e5bd275c6e7bb9bb9a816d237
Zenobius Jiricek <zenobius.jiricek@gmail.com>
2012-06-13 19:34:47 +0930

    fix indentation

--- longeruserfields/conf.py
+++ longeruserfields/conf.py
@@ -2,14 +2,10 @@
 from appconf import AppConf
 
 class ApplicationSettings(AppConf):
-	"""
-		Overridable Application Settings
-
-		more info : https://github.com/jezdez/django-appconf
-
-	"""
+	""" Overridable Application Settings
+		more info : https://github.com/jezdez/django-appconf """
 	USERNAME_LENGTH = 36 # length of a UUID1
 	EMAIL_LENGTH = 255 # maximum possible length of an email address
 
-    class Meta:
-        prefix = 'longeruserfields'
+	class Meta:
+		prefix = 'longeruserfields'
